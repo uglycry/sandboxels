950935d563473a6c635424884fd8c41ddb21339e
uglycry <149189398+uglycry@users.noreply.github.com>
2024-01-09 12:20:39 -0600

    Create gyatt.js

---
+++ mods/gyatt.js
@@ -0,0 +1,47 @@
+
+let rPOWDER = behaviors.POWDER
+console.log("Welcome to the console.");
+console.log(rPOWDER);
+elements.test = {
+    name: "Alex",
+    color: "#00FFF2",
+    behavior: behaviors.POWDER,
+    category: "powder",
+    state: "solid",
+    density: 19,
+    temp: 1e23,
+    tempHigh: 1e88,
+    stateHigh: "molten_alex",
+    reactions: {
+      
+    },
+};
+elements.molten_alex = {
+    name:"Liquid Alex",
+    color:"#0000ff",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    density: 10,
+    temp: 9.9e99,
+    tempHigh: 9.99e999,
+    stateHigh: "alex_gas",
+    tempLow: 9.9e1,
+    stateLow: "test",
+    reactions: {
+    },
+};
+
+elements.testium_gas = {
+    name:"Liquid Testium",
+    color:"#00ff00",
+    behavior: behaviors.GAS,
+    category: "gases",
+    state: "gas",
+    density: 5,
+    temp: 99.99e99,
+    tempLow: 450e999,
+    stateLow: "molten_alex",
+    reactions: {
+       
+    },
